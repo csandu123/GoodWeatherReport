a44efb95471072be44568c6663479c166c586484
Csandu <ces.pnw@gmail.com>
2022-12-23 22:41:32 -0800

    fully working

---
+++ script.js
@@ -0,0 +1,157 @@
+$(document).ready(function () {
+
+    // OpenWeather API
+    const apiKey = 'da53f5e8985401a752faa31cb46debf2';
+
+    const cityEl = $('h2#city');
+    const dateEl = $('h3#date');
+    const weatherIconEl = $('img#weather-icon');
+    const temperatureEl = $('span#temperature');
+    const humidityEl = $('span#humidity');
+    const windEl = $('span#wind');
+    const cityListEl = $('div.cityList');
+    const cityInput = $('#city-input');
+
+   // Store cities
+   let pastCities = [];
+
+   function compare(a, b) {
+       const cityA = a.city.toUpperCase();
+       const cityB = b.city.toUpperCase();
+
+       let comparison = 0;
+       if (cityA > cityB) {
+           comparison = 1;
+       } else if (cityA < cityB) {
+           comparison = -1;
+       }
+       return comparison;
+   }
+
+
+    // Load cities from local sotrage
+    function loadCities() {
+        const storedCities = JSON.parse(localStorage.getItem('pastCities'));
+        if (storedCities) {
+            pastCities = storedCities;
+        }
+    }
+
+    // Store searched cities in local storage
+    function storeCities() {
+        localStorage.setItem('pastCities', JSON.stringify(pastCities));
+    }
+
+   // Build URL for API call
+ 
+    function buildURLFromInputs(city) {
+        if (city) {
+            return `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+        }
+    }
+
+    function buildURLFromId(id) {
+        return `https://api.openweathermap.org/data/2.5/weather?id=${id}&appid=${apiKey}`;
+    }
+
+     // Display last 5 searched cities
+     function displayCities(pastCities) {
+        cityListEl.empty();
+        pastCities.splice(5);
+        let sortedCities = [...pastCities];
+        sortedCities.sort(compare);
+        sortedCities.forEach(function (location) {
+            let cityDiv = $('<div>').addClass('col-12 city');
+            let cityBtn = $('<button>').addClass('btn btn-light city-btn').text(location.city);
+            cityDiv.append(cityBtn);
+            cityListEl.append(cityDiv);
+        });
+    }
+    
+
+
+    // Search for weather conditions 
+    function searchWeather(queryURL) {
+
+        $.ajax({
+            url: queryURL,
+            method: 'GET'
+        }).then(function (response) {
+
+            // Store current city in past cities and remove duplicate
+            let city = response.name;
+            let id = response.id;
+    
+            if (pastCities[0]) {
+                pastCities = $.grep(pastCities, function (storedCity) {
+                    return id !== storedCity.id;
+                })
+            }
+            pastCities.unshift({ city, id });
+            storeCities();
+            displayCities(pastCities);
+            
+            cityEl.text(response.name);
+            let formattedDate = moment.unix(response.dt).format('L');
+            dateEl.text(formattedDate);
+            let weatherIcon = response.weather[0].icon;
+            weatherIconEl.attr('src', `http://openweathermap.org/img/wn/${weatherIcon}.png`).attr('alt', response.weather[0].description);
+            temperatureEl.html(((response.main.temp - 273.15) * 1.8 + 32).toFixed(1));
+            humidityEl.text(response.main.humidity);
+            windEl.text((response.wind.speed * 2.237).toFixed(1));
+
+            let lat = response.coord.lat;
+            let lon = response.coord.lon;
+            let queryURLAll = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+            $.ajax({
+                url: queryURLAll,
+                method: 'GET'
+            })
+        });
+    }
+
+     // Function to display the last searched city
+     function displayLastSearchedCity() {
+        if (pastCities[0]) {
+            let queryURL = buildURLFromId(pastCities[0].id);
+            searchWeather(queryURL);
+        } else {
+            // if no past searched cities, load Seattle weather data
+            let queryURL = buildURLFromInputs("Seattle");
+            searchWeather(queryURL);
+        }
+    }
+ 
+    // Click handler for search button
+    $('#search-btn').on('click', function (event) {
+
+        event.preventDefault();
+
+        let city = cityInput.val().trim();
+        city = city.replace(' ', '%20');
+
+        cityInput.val('');
+
+        // Build the query url with the city and searchWeather
+        if (city) {
+            let queryURL = buildURLFromInputs(city);
+            searchWeather(queryURL);
+        }
+    }); 
+    
+    // load city weather
+    $(document).on("click", "button.city-btn", function (event) {
+        let clickedCity = $(this).text();
+        let foundCity = $.grep(pastCities, function (storedCity) {
+            return clickedCity === storedCity.city;
+        })
+        let queryURL = buildURLFromId(foundCity[0].id)
+        searchWeather(queryURL);
+    });
+
+
+    loadCities();
+    displayCities(pastCities);
+    displayLastSearchedCity();
+
+});
